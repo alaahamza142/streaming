b1bc1f8ee340b1fa8c4bf8ef860cece38f0b1ce1
alaahamza142 <47693798+alaahamza142@users.noreply.github.com>
2019-02-16 21:56:00 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : Retir'ed ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(` بيلين انا باكل `,"http://twitch.tv/Death Shop")
+client.user.setGame(` لقد قتلت من قبل كنده `,"http://twitch.tv/Death Shop")
 client.user.setStatus("Online")
 });
 
